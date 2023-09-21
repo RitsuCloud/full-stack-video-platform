ad69c8cd1676d176300a928c0c9b990009143e4b
RitsuCloud <chonhinchou@gmail.com>
2023-09-20 17:27:58 -0700

    added error message for internal video conversion

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -5,8 +5,24 @@ import Ffmpeg from "fluent-ffmpeg";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  // Get path of the input video from the request body
+  const inputFilePath = req.body.inputFilePath
+  const outputFilePath = req.body.outputFilePath
+
+  if (!inputFilePath || !outputFilePath){
+    res.status(400).send("Bad request: Missing file path.") // clien error, gave up the wrong request
+  }
+
+  Ffmpeg(inputFilePath)
+  .outputOptions("-vf", "scale=-1:360") // converting the video into 360p, vf = video file?
+  .on("end", () => {
+
+  })
+  .on("erorr", (err) => {
+    console.log(`An error occured: ${err.message}`);
+    res.status(500).send(`Internal Server Erorr ${err.message}`);
+  });
 });
 
 app.listen(port, () => {
